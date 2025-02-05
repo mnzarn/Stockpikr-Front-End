5c1b91e6e1efe929ef561b9ed9b85ea6b57264fd
sammy <123290749+szayadi@users.noreply.github.com>
2025-02-04 19:49:20 -0800

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,40 @@
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import LandingPage from './components/LandingPage';
 import Positions from './components/Positions/Positions';
+import SignedOutPositions from './components/Positions/SignedOutPositions';
 import Settings from './components/Settings/Settings';
 import SignIn from './components/SignIn';
 import { StockQuotePage } from './components/Stock/StockQuotePage';
 import SwaggerDocs from './components/Swagger/SwaggerDocs';
 import Watchlist from './components/Watchlist/Watchlist';
+import { UserApiService } from './services/UserApiService';
+
+const ProtectedPositions = () => {
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkUserLoggedIn = async () => {
+      const loggedIn = await UserApiService.isUserLoggedIn();
+      console.log('isUserLoggedIn: ', loggedIn);
+      setIsUserLoggedIn(loggedIn);
+      setIsLoading(false);
+    };
+
+    checkUserLoggedIn();
+  }, []);
+
+  return isUserLoggedIn ? <Positions /> : <SignedOutPositions />;
+};
 
 const routes = () => (
   <Routes>
     <Route path="/" element={<LandingPage />} />
     <Route path="/watchlist" element={<Watchlist />} />
     <Route path="/dashboard" element={<Dashboard />} />
-    <Route path="/positions" element={<Positions />} />
+    <Route path="/positions" element={<ProtectedPositions />} />
     <Route path="/settings" element={<Settings />} />
     <Route path="/signin" element={<SignIn />} />
     <Route path="/swaggerDocs" element={<SwaggerDocs />} />
