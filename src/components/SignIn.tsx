ce2a41a644769c1cdea0fd861814c7bb93fa4d62
Arnold Manzi <93343042+mnzarn@users.noreply.github.com>
2025-02-12 14:21:02 -0800

    Update SignIn.tsx

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -32,7 +32,7 @@ export default function SignIn() {
           size="large"
           color="inherit"
           variant="outlined"
-          href="https://stockpikr2.azurewebsites.net/login/federated/google"
+          href="https://agreeable-ground-08e4a8b1e.4.azurestaticapps.net/auth/google/callback
           startIcon={<GoogleIcon />}
         >
           Sign In with Google
