67519e3b58b956448f0da78ada0102d0621cd04c
Harpreet Sangha <hasangha04@gmail.com>
2025-02-25 16:21:35 -0800

    Load Firebase API keys dynamically

--- src/services/FirebaseConfig.ts
+++ src/services/FirebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: window.ENV?.REACT_APP_FIREBASE_API_KEY,
-  authDomain: window.ENV?.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: window.ENV?.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: window.ENV?.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: window.ENV?.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: window.ENV?.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 console.log("Firebase API Key:", firebaseConfig.apiKey);
